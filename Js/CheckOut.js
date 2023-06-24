0757273385f5e7d32def441fa58489ca5c1e19c3
rajaasim4 <rajaasim4474@gmail.com>
2023-06-24 22:26:55 +0500

    Checkout Page Added

---
+++ Js/CheckOut.js
@@ -0,0 +1,322 @@
+//*Array Data
+
+let ProductData = [
+    {
+      id: 1,
+      name: "Sunstar Fresh Melon Juice",
+      rating: "4.5",
+      prevPrice: 22,
+      price: 10.0,
+      imgsrc: "../Assets/sunstar_fresh_melon_juice.png",
+    },
+    {
+      id: 2,
+      name: "Fresh local tomatoes",
+      rating: "4.3",
+      prevPrice: 8,
+      price: 5,
+      imgsrc: "../Assets/Fresh_local_tomatoes.png",
+    },
+    {
+      id: 3,
+      name: "Fresh Cucumber",
+      rating: "4.5",
+      prevPrice: 9,
+      price: 4,
+      imgsrc: "../Assets/Fresh_cucmber.png",
+    },
+    {
+      id: 4,
+      name: "Heinz tomato ketchup",
+      rating: "5.0",
+      prevPrice: 12,
+      price: 9,
+      imgsrc: "../Assets/Heniz_tomato_ketchup.png",
+    },
+    {
+      id: 5,
+      name: "Local Avocado",
+      rating: "4.7",
+      prevPrice: 8,
+      price: 4,
+      imgsrc: "../Assets/Local_avacado.png",
+    },
+    {
+      id: 6,
+      name: "Munch Crunchy Cookies",
+      rating: "4.5",
+      prevPrice: 10,
+      price: 8,
+      imgsrc: "../Assets/Munch_crunchy_cookies.png",
+    },
+    {
+      id: 7,
+      name: "Fresh Bananas",
+      rating: "5",
+      prevPrice: 15,
+      price: 10,
+      imgsrc: "../Assets/Fresh_bananas.png",
+    },
+    {
+      id: 8,
+      name: "Fresh Orange Juice",
+      rating: "4.5",
+      prevPrice: 15,
+      price: 12,
+      imgsrc: "../Assets/Fresh_orange_juice.png",
+    },
+    {
+      id: 9,
+      name: "Pure Dairy milk ",
+      rating: "5",
+      prevPrice: 8,
+      price: 4,
+      imgsrc: "../Assets/Pure_dairy_milk.png",
+    },
+    {
+      id: 10,
+      name: "Raspberries",
+      rating: "5",
+      prevPrice: 20,
+      price: 15,
+      imgsrc: "../Assets/Raspberries.png",
+    },
+    {
+      id: 11,
+      name: "French Cheese Lays",
+      rating: "5",
+      prevPrice: 5,
+      price: 3,
+      imgsrc: "../Assets/Lays.png",
+    },
+    {
+      id: 12,
+      name: "Amber pure honey",
+      rating: "4.5",
+      prevPrice: 25,
+      price: 20,
+      imgsrc: "../Assets/Amber_pure_honey.png",
+    },
+    {
+      id: 13,
+      name: "Pharma Cashew butter",
+      rating: "5",
+      prevPrice: 30,
+      price: 25,
+      imgsrc: "../Assets/Pharma_cashew_butter.png",
+    },
+    {
+      id: 14,
+      name: "Suzane creamy Cupcake",
+      rating: "4.2",
+      prevPrice: 15,
+      price: 11,
+      imgsrc: "../Assets/Suzane_creamy_cupcake.png",
+    },
+    {
+      id: 15,
+      name: "1.5 Liter Coca Cola",
+      rating: "5",
+      prevPrice: 9,
+      price: 4,
+      imgsrc: "../Assets/Coca_cola.jpg",
+    },
+    {
+      id: 16,
+      name: "Mango",
+      rating: "5",
+      prevPrice: 11,
+      price: 7,
+      imgsrc: "../Assets/Mango.png",
+    },
+  ];
+  
+  let BestSellingProductsData = [
+    {
+      id: 17,
+      name: "Fresh Lemon",
+      rating: "4.5",
+      prevPrice: 25,
+      price: 20,
+      imgsrc: "../Assets/Fresh_lemon.png",
+    },
+    {
+      id: 18,
+      name: "Apple Juice",
+      rating: "4.3",
+      prevPrice: 8,
+      price: 5,
+      imgsrc: "../Assets/Apple_juice.png",
+    },
+    {
+      id: 19,
+      name: "Katz White Bread",
+      rating: "4.5",
+      prevPrice: 9,
+      price: 4,
+      imgsrc: "../Assets/Katz_white_bread.png",
+    },
+    {
+      id: 20,
+      name: "Eminent Chocolate Cookies",
+      rating: "5.0",
+      prevPrice: 12,
+      price: 9,
+      imgsrc: "../Assets/Eminent_chocolate_cookies.png",
+    },
+    {
+      id: 21,
+      name: "Organic Butter",
+      rating: "4.7",
+      prevPrice: 8,
+      price: 4,
+      imgsrc: "../Assets/Organic_butter.png",
+    },
+    {
+      id: 22,
+      name: "Local Honey",
+      rating: "4.5",
+      prevPrice: 10,
+      price: 8,
+      imgsrc: "../Assets/Local_honey.png",
+    },
+  ];
+  let JustLandedProductsData = [
+    {
+      id: 23,
+      name: "Jelly beans exxotics",
+      rating: "4.5",
+      prevPrice: 22,
+      price: 10.0,
+      imgsrc: "../Assets/Jelly_beans_exotics.png",
+    },
+    {
+      id: 24,
+      name: "Growers cider",
+      rating: "4.3",
+      prevPrice: 8,
+      price: 5,
+      imgsrc: "../Assets/Growers_cider.png",
+    },
+    {
+      id: 25,
+      name: "Fresh Grapes",
+      rating: "4.5",
+      prevPrice: 9,
+      price: 4,
+      imgsrc: "../Assets/Fresh_grapes.png",
+    },
+    {
+      id: 26,
+      name: "Fresh Pineapple",
+      rating: "5.0",
+      prevPrice: 12,
+      price: 9,
+      imgsrc: "../Assets/Fresh_pineapple.png",
+    },
+    {
+      id: 27,
+      name: "Fresh Pomegranate",
+      rating: "4.7",
+      prevPrice: 8,
+      price: 4,
+      imgsrc: "../Assets/Fresh_pomegranate.png",
+    },
+    {
+      id: 28,
+      name: "Fresh Dates",
+      rating: "4.5",
+      prevPrice: 10,
+      price: 8,
+      imgsrc: "../Assets/Fresh_dates.png",
+    },
+  ];
+  let TopOfferedProducts = [
+    {
+      id: 29,
+      name: "Pacific barista beverage",
+      rating: "4.5",
+      prevPrice: 22,
+      price: 10.0,
+      imgsrc: "../Assets/Pacific_barista_beverage.png",
+    },
+    {
+      id: 30,
+      name: "Kombucha local lilikoi",
+      rating: "4.3",
+      prevPrice: 8,
+      price: 5,
+      imgsrc: "../Assets/Kombucha_local_lilikoi.png",
+    },
+    {
+      id: 31,
+      name: "Oat milk barista",
+      rating: "4.5",
+      prevPrice: 9,
+      price: 4,
+      imgsrc: "../Assets/Oat_milk_barista.png",
+    },
+    {
+      id: 32,
+      name: "DairyMilk Chocolate",
+      rating: "5.0",
+      prevPrice: 12,
+      price: 9,
+      imgsrc: "../Assets/Dairymilk_chocolate.png",
+    },
+    {
+      id: 33,
+      name: "Dr. raw relax hearts",
+      rating: "4.7",
+      prevPrice: 8,
+      price: 4,
+      imgsrc: "../Assets/Dr_raw_relax_hearts.png",
+    },
+    {
+      id: 34,
+      name: "Fresh Brocolli",
+      rating: "4.5",
+      prevPrice: 10,
+      price: 8,
+      imgsrc: "../Assets/Fresh_brocolli.png",
+    },
+  ];
+  
+  //*All Data Combining to a Array
+  
+  let allProductsData = [
+    ...BestSellingProductsData,
+    ...JustLandedProductsData,
+    ...ProductData,
+    ...TopOfferedProducts,
+  ];
+  
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+function totalAmount() {
+    if (basket.lengt !== 0) {
+      let amount = basket
+        .map((val) => {
+          let { id, quantity } = val;
+          let search = allProductsData.find((x) => x.id === id) || [];
+          return quantity * search.price;
+        })
+        .reduce((x, y) => x + y, 0);
+      document.querySelector("#subTotal").innerHTML = amount;
+      document.querySelector("#cartTotalPrice").innerHTML = `$${amount}`;
+      document.querySelector("#totalAmount").innerHTML = amount + 15;
+    } else return;
+  }
+  totalAmount();
+
+  let calculateItemsQuantity = () => {
+    let cartTotalQuntity = document.querySelector("#cartTotalQuantity");
+    let totalQuantity = basket
+      .map((val) => val.quantity)
+      .reduce((x, y) => x + y, 0);
+    cartTotalQuntity.innerHTML = totalQuantity;
+    generateCartItem();
+    totalAmount();
+  };
+  
